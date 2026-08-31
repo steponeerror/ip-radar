e1d8773f1eed258ffa4f685134c464b2688dc265
steponeerror <huxiao0207@qq.com>
2026-09-01 02:22:17 +0800

    lineage: greensnow joins DERIVED_SOURCES + eval cluster (OC=1.0 mirror, spec Part 6, Q9-G1)

--- backend/ipdb/_eval/config.py
+++ backend/ipdb/_eval/config.py
@@ -29,6 +29,7 @@ LINEAGE_CLUSTERS = {
     "firehol": "aggregated-threat",
     "ipsum":   "aggregated-threat",
     "otx":     "aggregated-threat",
+    "greensnow": "aggregated-threat",  # OC=1.0 w/ firehol, containment ≥0.9 (2026-09-01)
 }
 
 MODEL_W = 10        # G1' prior strength
